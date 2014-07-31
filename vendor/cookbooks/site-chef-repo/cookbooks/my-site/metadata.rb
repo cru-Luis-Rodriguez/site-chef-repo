7fdcb737ff8666f750ea6407fb94dd8bcd64fa4c
Luis Rodriguez <luis.rodriguez@cru.org>
2014-07-31 10:05:36 -0400

    First commit

---
+++ vendor/cookbooks/site-chef-repo/cookbooks/my-site/metadata.rb
@@ -0,0 +1,8 @@
+name             'my-site'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures my-site'
+long_description 'Installs/Configures my-site'
+version          '0.1.0'
+
